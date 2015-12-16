d8d8473f40183b258a40146c774b7375c088fabb
LacieJ <jolene.fan@hotmail.com>
2015-12-16 09:49:34 +0800

    biathlon assets + codes

---
+++ index.php
@@ -0,0 +1,338 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+		<title>NUS Biathlon 2013</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+		<meta name="description" content="NUS Biathlon is organized by the NUS Students' Sports Club. This annual event is one of the main highlights on the NUS sporting scene. It an opportunity for everyone whether you're an NUS staff, student or a member of the public. If you're a trained athlete wanting to compete or a newcomer simply looking for a challenge, you're more than welcomed to join us!" />
+		<meta name="keywords" content="biathlon, nus, nusssc, nus sports club, nus biathlon, run, swim, triathlon, wet, dry" />
+		<meta name="author" content="NUS Biathlon" />
+		<link rel="shortcut icon" href="images/favIcon/favicon.ico"> 
+		<link rel="stylesheet" type="text/css" href="css/demo.css" />
+		<link rel="stylesheet" type="text/css" href="css/style3.css" />
+		<link rel="stylesheet" type="text/css" href="css/enlargeImg.css" />
+		<link href='http://fonts.googleapis.com/css?family=Electrolize' rel='stylesheet' type='text/css' />
+		
+			
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.js"></script>
+	<script src="js/galleria/galleria-1.2.9.min.js"></script>
+	<script src="js/galleria/plugins/facebook/galleria.facebook.js"></script>
+		
+	</head>
+	<body>	
+		<!-- Codrops top bar -->
+		<div class="codrops-top">
+			<a href="http://www.nussportsclub.org/">
+				<strong>&laquo; Visit:</strong>NUS Students' Sports Club
+			</a>
+			<span class="right">
+				<a href="">
+					<strong></strong>
+				</a>
+			</span>
+			<div class="clr"></div>
+		</div><!--/ Codrops top bar -->
+				
+		<!-- Home -->
+		<div id="home" class="content">
+			<h2>WELCOME</h2>
+			<p>A race not to be missed!</p>
+			<br/>
+			<ul id="works">
+				<li>
+					<img src="http://i1304.photobucket.com/albums/s534/nussportsclub/Biathlon%202013/comingapril_zps51167f5c.jpg" border="0" alt=" photo comingapril_zps51167f5c.jpg" width="250px"/>
+				</li>
+				<li>
+					<img src="http://i1304.photobucket.com/albums/s534/nussportsclub/Biathlon%202013/biathlontoprint2_zps9e5c048e.jpg" border="0" alt=" photo biathlontoprint2_zps9e5c048e.jpg" width="250px"/></a>
+				</li>
+			</ul>
+				 
+		</div>
+		<!-- /Home -->
+		
+		<!-- About Biathlon -->
+		<div id="aboutBiathlon" class="panel">
+			<div class="content">
+				<h2>About NUS Biathlon 2013</h2>
+				<p>NUS Biathlon is organized by the NUS Students' Sports Club. This annual event is one of the main highlights on the NUS sporting scene.</p>
+				<p>It an opportunity for everyone whether you're an NUS staff, student or a member of the public. If you're a trained athlete wanting to compete or a newcomer simply looking for a challenge, you're more than welcomed to join us! </p>
+			</div>
+		</div>
+		<!-- /About Biathlon -->
+		
+		
+		<!-- Race Info -->
+		<div id="raceInfo" class="panel">
+			<div class="content">
+				<h2>Race Information</h2>
+				<p>When: 6 April 2013, 6.30AM – 2.00PM</p>
+				<p>Where: NUS Sports and Recreation Centre</p>
+				<br/>
+				<br/>
+				<p>Categories:</p>
+				<br/>
+				<p><u>Open (Swim 16 Laps & 5 km run)</u><br/>
+				Normal: $24<br/>
+				Early Bird: $20<br/>
+				- Open Individual Men<br/>
+				- Open Individual Women</p>
+				<p><u>Novice (Swim 8 Laps & 3 km run)</u><br/>
+				Normal: $20<br/>
+				Early Bird: $16<br/>
+				- Novice Individual Men<br/>
+				- Novice Individual Women</p>
+				<p><u>Mixed Relay (Swim 8 laps & 3 km run)</u><br/>
+				Normal: $20/person<br/>
+				Early Bird: $16/person<br/>
+				- 1 Male and 1 female pair. Each will either take swimming or running.</p>
+				<br/>
+				<br/>
+				<p>Registration has opened and early bird’s special is available until 11th March.<br/>
+				Registration is based on a first come first serve basis.</p>
+				<br/>
+				<br />
+				<p>Race Pack:</p>
+				<br />
+				<p>Race pack and shirts will be given on the actual day of the event.</p>
+				<br/>
+				<br/>
+				<p>Race Route</p>
+				<br/>
+				<table>
+					<tr>
+						<td>
+							<p>3 KM</p>
+							<div class="hovergallery">
+								<img src="http://i1304.photobucket.com/albums/s534/nussportsclub/Biathlon%202013/route/3km_zpse0dca2d3.jpg" border="0" alt=" photo 3km_zpse0dca2d3.jpg"/ width="400">
+							</div>
+						</td>
+						<td>
+							<p>5 KM</p>
+							<div class="hovergallery">
+								<img src="http://i1304.photobucket.com/albums/s534/nussportsclub/Biathlon%202013/route/5km_zps24dd7de4.jpg" border="0" alt=" photo 5km_zps24dd7de4.jpg"/ width="400">
+							</div>
+						</td>
+					</tr>
+				</table>
+			</div>
+		</div>
+		<!-- /Race Info -->
+		
+		<!-- Runner Info -->
+		<div id="runnerInfo" class="panel">
+			<div class="content">
+				<h2>Runner's Information</h2>
+				<p>Registered runners will receive an email of confirmation and details about the swim trial. Payment will also be made on the day of swim trial.</p>
+				<p>The swim trial will be held on:</br>
+				- 22nd March, Friday: 9 to 11am, 2pm to 4pm</br>
+				- 23rd March, Saturday: 9 to 11am</p></br>
+				<p>Participants who have a certificate or any evidence showing that they can swim for the past 6 months can be exempted from the swim trial. Cash or cheque payment is still required on the swim trial dates.</p>
+				<h2>Runner's Wave and Reporting Time</h2>
+				<p>Click <a href="https://docs.google.com/spreadsheet/ccc?key=0AtqrmhE_IAR8dERwSTk5QzFYcGNEbHFQRzRUY2l1ZEE&usp=sharing" target="_blank">here</a> to find out your wave and reporting time.</p>
+				<h2>Race Booklet</h2>
+				<p>Prepare yourself for the ultmiate race with our handy race guide <a href="https://docs.google.com/file/d/0B9qrmhE_IAR8MWlDNTNkQlNMalk/edit?usp=sharing" target="_blank">here</a>.</p>
+			</div>
+		</div>
+		<!-- /Runner Info -->
+		
+		<!-- Volunteer Info -->
+		<div id="volunteer" class="panel">
+			<div class="content">
+				<h2>Be A Volunteer!</h2>
+				<p>NUS Biathlon is here again and we need your help to make it a success!</b>
+
+				<p>We are looking for 100 volunteers to be road marshalls, stage crew, timekeepers and water point helpers.<br/>
+				As the event starts at 6.30am on 6 April, volunteers are required to report on the night before the event. Volunteers will stay over in MPSH 6. Registration of volunteers will start at 6.30pm on 5 April.</br/></p>
+
+				<p>Date: 5 & 6 April 2013<br />
+				Time: 6.30pm(5 April) - 2.00pm(6 April)<br />
+				Venue: MPSH 6 <br/></p>
+
+				<p>Breakfast and lunch will be provided. Volunteers will also receive a special edition of the biathlon t-shirt.<br/></p>
+
+				<p>If you have any enquiries, please contact Jane at 98152248.<br/></p>
+				
+				<p>If you are interested to become part of this event as a volunteer, do fill up the volunteers recruitment form below.</p><br /><br/>
+				
+				<iframe src="https://docs.google.com/forms/d/1oD5qhIK8HATiLWBVnIQ6OQFmdBTvg8JqPyBJKicq1IY/viewform?embedded=true" width="100%" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+				
+			</div>
+		</div>
+		<!-- /Vunner Info -->
+		
+		<!-- Gallery -->
+
+		<script>
+				Galleria.loadTheme("js/themes/classic/galleria.classic.min.js");
+				Galleria.configure({
+				    lightbox: true
+				});
+				Galleria.run(#galleria);
+		</script>
+
+		<div id="gallery" class="panel">
+			<div class="content">
+				<h2>Gallery</h2>
+				<p>NUS Biathlon 2012</p>
+				<script>
+				Galleria.loadTheme('js/galleria/themes/classic/galleria.classic.min.js');
+				Galleria.run('#galleria1', {
+				 facebook: 'album:324897690915849',
+				 width: 745,
+				 height: 400,
+				 lightbox: true});
+				</script>
+				<div id="galleria1"></div>
+				
+				<br/>
+				
+				<script>
+				Galleria.loadTheme('js/galleria/themes/classic/galleria.classic.min.js');
+				Galleria.run('#galleria2', {
+				 facebook: 'album:367315586674059',
+				 width: 745,
+				 height: 400,
+				 lightbox: true});
+				</script>
+			<div id="galleria2"></div>
+			</div>
+		</div>
+		<!-- /Gallery -->
+		
+		<!-- Sponsor Info -->
+		<div id="sponsor" class="panel">
+			<div class="content">
+				<h2>Our Sponsors</h2>
+				<p>Gold Sponsor</p>
+				<ul id="works">
+						<li>
+							<img src="http://i1304.photobucket.com/albums/s534/nussportsclub/Biathlon%202013/sponsors/arena_zpsecbef596.jpg" border="0" alt=" photo arena_zpsecbef596.jpg" width="300" height="180"/>
+						</li>
+					<a href="http://www.facebook.com/GarnierSingapore" target="_blank"/>
+						<li>
+							<img src="http://i1304.photobucket.com/albums/s534/nussportsclub/Biathlon%202013/sponsors/garnier_zps45399c5c.jpg" border="0" alt=" photo garnier_zps45399c5c.jpg" width="300" height="180"/>
+						</li>
+					</a>
+					<a href="http://www.facebook.com/GarnierSingapore" target="_blank"/>
+						<li>
+							<img src="http://i1304.photobucket.com/albums/s534/nussportsclub/Biathlon%202013/sponsors/garniernew_zpsc80f3684.jpg" border="0" alt=" photo garniernew_zpsc80f3684.jpg" width="300"/>
+						</li>
+					</a>
+					<a href="http://www.atlanticoptical.biz/home.html" target="_blank">
+						<li>
+							<img src="http://i1304.photobucket.com/albums/s534/nussportsclub/Biathlon%202013/sponsors/atlantic_zps555452d0.jpg" border="0" alt=" photo atlantic_zps555452d0.jpg"width="300"/>
+						</li>
+					</a>
+						<li>
+							<img src="http://i1304.photobucket.com/albums/s534/nussportsclub/Biathlon%202013/sponsors/butterfactory_zps439cca3a.jpg" border="0" alt=" photo butterfactory_zps439cca3a.jpg" height="213"/>
+						</li>
+					<a href="http://strictlypancakes.com.sg/" target="_blank">
+						<li>
+							<img src="http://i1304.photobucket.com/albums/s534/nussportsclub/Biathlon%202013/sponsors/pancakes_zps46084387.jpg" border="0" alt=" photo pancakes_zps46084387.jpg" height="213"/>
+						</li>
+					</a>
+					<a href="http://www.alphico.com.sg/brands.php?id=14" target="_blank">
+						<li>
+							<img src="http://i1304.photobucket.com/albums/s534/nussportsclub/Biathlon%202013/sponsors/block_zps48675292.jpg" border="0" alt=" photo block_zps48675292.jpg" height="213"/>
+						</li>
+					</a>
+					<a href="http://www.alphico.com.sg/brands.php?id=6" target="_blank">
+						<li>
+							<img src="http://i1304.photobucket.com/albums/s534/nussportsclub/Biathlon%202013/sponsors/fantastic_zpsf8d64971.jpeg" border="0" alt=" photo fantastic_zpsf8d64971.jpeg" height="213"/>
+						</li>
+					</a>
+					<a href="http://www.juronglakerun.com/" targrt="_blank">
+						<li>
+							<img src="http://i1304.photobucket.com/albums/s534/nussportsclub/Biathlon%202013/sponsors/JURONG-LAKE-RUN-2013_zps1967c1c0.png" border="0" alt=" photo bizcom_zpsb925ff30.jpg"/ height="213">
+						</li>
+					</a>
+					<a href="http://www.alphico.com.sg/brands.php?id=13" targrt="_blank">
+						<li>
+							<img src="http://i1304.photobucket.com/albums/s534/nussportsclub/Biathlon%202013/sponsors/fote_zpsd57a8970.jpg" border="0" alt=" photo fote_zpsd57a8970.jpg"/ height="100">
+						</li>
+					</a>
+					<a href="http://www.alphico.com.sg/brands.php?id=5" targrt="_blank">
+						<li>
+							<img src="http://i1304.photobucket.com/albums/s534/nussportsclub/Biathlon%202013/sponsors/MameeLogo_zps57858092.jpg" border="0" alt=" photo nusstyle_zps79d9af7b.jpg" height="100"/>
+						</li>
+					</a>
+					<a href="http://nussu.org.sg/nustyle/" targrt="_blank">
+						<li>
+							<img src="http://i1304.photobucket.com/albums/s534/nussportsclub/Biathlon%202013/sponsors/nusstyle_zps79d9af7b.jpg" border="0" alt=" photo nusstyle_zps79d9af7b.jpg" width="320"/>
+						</li>
+					</a>
+					<a href="http://nussu.org.sg/nustyle/" targrt="_blank">
+						<li>
+							<img src="http://i1304.photobucket.com/albums/s534/nussportsclub/Biathlon%202013/sponsors/bizcom_zpsb925ff30.jpg" border="0" alt=" photo bizcom_zpsb925ff30.jpg"/ height="100">
+						</li>
+					</a>
+				</ul>
+				<p>Silver Sponsor</p>
+				<ul id="works">
+					<a href="http://www.nutritionpark.com/" target="_blank">
+						<li>
+							<img src="http://i1304.photobucket.com/albums/s534/nussportsclub/Biathlon%202013/sponsors/np_zpsbac01d9c.jpg" border="0" alt=" photo np_zpsbac01d9c.jpg" width="450"/>
+						</li>
+					</a>
+				</ul>
+				<p>Bronze Sponsor</p>
+				<ul id="works">
+					<a href="http://www.gardenia.com.sg/" target="_blank">
+						<li>
+							<img src="http://i1304.photobucket.com/albums/s534/nussportsclub/Biathlon%202013/sponsors/gardenia_zpsd87b165e.jpg" border="0" alt=" photo gardenia_zpsd87b165e.jpg" width="350"/>
+						</li>
+					</a>
+				</ul>
+			</div>
+		</div>
+		<!-- /Sponsor Info -->
+		
+		<!-- Contact Us -->
+		<div id="contact" class="panel">
+			<div class="content">
+				<h2>Contact Us</h2>
+				<table>
+					<tr>
+						<td align="center">
+							<img src="images/logo/facebook.png">
+						</td>
+						<td>
+							<p><a href="https://www.facebook.com/pages/NUS-Biathlon-2013/269308473141438?fref=ts">NUS Biathlon 2013</a></p>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<img src="images/logo/email.png" width="100px">
+						</td>
+						<td>
+							<p><a href="mailto:nusbiathlon2013@gmail.com">Email: nusbiathlon2013@gmail.com</a></p>	
+						</td>
+					</tr>
+				</table>				
+			</div>
+		</div>
+		<!-- /Sponsor Info -->
+		
+		<!-- Header with Navigation -->
+		<div id="header">
+			<h1><img src="images/logo/logo.png" width="200px"></h1>
+			<ul id="navigation">
+				<li><a id="link-home" href="#home">Home</a></li>
+				<li><a id="link-aboutBiathlon" href="#aboutBiathlon">About NUS Biathlon</a></li>
+				<li><a id="link-raceInfo" href="#raceInfo">Race & Route Details</a></li>
+				<li><a id="link-runnerInfo" href="#runnerInfo">Runner's Information</a></li>
+				<li><a id="link-volunteer" href="#volunteer">Be A Volunteer!</a></li>
+				<li><a id="link-gallery" href="#gallery">Photo Gallery</a></li>
+				<li><a id="link-sponsor" href="#sponsor">Our Sponsors</a></li>
+				<li><a id="link-contact" href="#contact">Contact Us</a></li>
+			</ul>
+		</div>
+	</div>
+	
+	<div style="position:absolute; top:50px; right:15px;">
+			<iframe src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fpages%2FNUS-Biathlon-2013%2F269308473141438%3Fnotif_t%3Dfbpage_admin&amp;width=292&amp;height=590&amp;show_faces=true&amp;colorscheme=light&amp;stream=true&amp;border_color&amp;header=true&amp;appId=185213241593314" scrolling="no" frameborder="0" style="background-color:white; border:none; overflow:hidden;height:540px; allowTransparency="true"></iframe>
+	</div>
+	
+	</body>
+</html>
---
+++ test.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<style>
+
+#leftBar {
+position: fixed;
+top: 0px;
+left: 0px;
+bottom: 0px;
+right: 1000px;
+border-right: 2px white;
+box-shadow: 10px 10px 5px #888888;
+}
+
+
+
+</style>
+
+</head>
+<body>
+<div id="leftBar">
+
+</div>
+</body>
+</html>
